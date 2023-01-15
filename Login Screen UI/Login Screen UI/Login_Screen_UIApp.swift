1a536e721049742869ee22ac03bb640ae16a48b8
clarke.ben <40464267+clarkeben@users.noreply.github.com>
2023-01-15 21:17:57 +0000

    Added the Login screen UI project

---
+++ Login Screen UI/Login Screen UI/Login_Screen_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_Screen_UIApp.swift
+//  Login Screen UI
+//
+//  Created by Ben Clarke on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Login_Screen_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
