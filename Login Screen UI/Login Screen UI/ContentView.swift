2f64f6449fe325839defc9de352a7ec9b2d2773e
clarke.ben <40464267+clarkeben@users.noreply.github.com>
2023-01-19 23:51:19 +0000

    Updated the login view with assets

--- Login Screen UI/Login Screen UI/ContentView.swift
+++ Login Screen UI/Login Screen UI/ContentView.swift
@@ -21,19 +21,63 @@ struct ContentView: View {
 }
 
 struct LoginView: View {
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
     @State private var email = ""
     @State private var password = ""
     
     var body: some View {
         VStack {
-            TextField("Email", text: $email)
-            SecureField("Password", text: $password)
-            Button {
-                ///Action
-            } label: {
+            
+            Image("login")
+            
+            HStack {
                 Text("Login")
+                    .font(.title)
+                Spacer()
+            }.padding([.leading, .trailing], 20)
+            
+            HStack {
+                Image(systemName: "lock.fill")
+                TextField("Email", text: $email)
             }
-
+            .padding(.vertical, 10)
+            .overlay(Rectangle().frame(height: 1).padding(.top, 35))
+            .foregroundColor(.gray)
+            .padding([.leading, .trailing], 20)
+            .padding(.bottom, 10)
+            
+            HStack {
+                Image(systemName: "lock.fill")
+                SecureField("Password", text: $password)
+                Text("Forgot?").foregroundColor(.blue)
+            }
+            .padding(.vertical, 10)
+            .overlay(Rectangle().frame(height: 1).padding(.top, 35))
+            .foregroundColor(.gray)
+            .padding([.leading, .trailing], 20)
+            .padding(.bottom, 20)
+            
+            Button("Login") {
+                ///Action
+            }
+            .frame(width: width-60, height: 50)
+            .foregroundColor(.white)
+            .background(.blue)
+            .cornerRadius(10)
+            
+            
+            Text("Or, login with..")
+                .foregroundColor(.gray)
+                .padding(40)
+            
+            HStack {
+                Text("New to the app?")
+                Button("Register"){
+                    
+                }
+            }
         }
     }
 }
