fc5cfed7cd336861944e1c5d5010275289dc107b
clarke.ben <40464267+clarkeben@users.noreply.github.com>
2023-01-27 23:20:47 +0000

    Added Password Validator project

---
+++ Password Validator/Password Validator/Password_ValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_ValidatorApp.swift
+//  Password Validator
+//
+//  Created by Ben Clarke on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Password_ValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
