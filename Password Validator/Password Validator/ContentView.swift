5dc7120a6f5716808bb4f4bbfccf8f83a7a49078
clarke.ben <40464267+clarkeben@users.noreply.github.com>
2023-01-29 17:38:08 +0000

    Created basic UI and added a view model with methods to handle password validation

--- Password Validator/Password Validator/ContentView.swift
+++ Password Validator/Password Validator/ContentView.swift
@@ -8,14 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    @State private var passwordText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Register")
+                    .font(.title)
+                
+                Text("Please create a secure password including the following criteria below")
+                    .font(.system(size: 12))
+                    .foregroundColor(.gray)
+                
+                HStack {
+                    TextField("Password", text: $passwordText)
+                    Spacer()
+                    Image(systemName: "eye")
+                        .foregroundColor(.gray)
+                }
+                .padding(10)
+                .border(.gray)
+                .cornerRadius(2)
+
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "chevron.right")
+                            .resizable()
+                            .foregroundColor(.white)
+                            .frame(width: 40)
+                    }
+                    .frame(width: 60, height: 60)
+                    .background(.purple)
+                    .clipShape(Circle())
+                }
+            }
+            .padding(10)
+            .navigationTitle("Register")
         }
-        .padding()
+    }
+}
+
+// MARK: - PasswordValidator View Model
+class PasswordValidatorViewModel: ObservableObject {
+    // Properties
+    @Published var passwordText = ""
+    @Published var showPassword = false
+    
+    
+    
+    // Methods
+    /// Check that the password is over 12 characters long
+    private func isOver12Characters(_ password: String) -> Bool {
+        if password.count >= 12 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    /// Check that the password contains one capital letter
+    private func containsCapitalLetter() -> Bool {
+         
+        return false
+    }
+    
+    /// Check that the password contains one number
+    private func containsNumbers() -> Bool {
+        
+        return false
+    }
+    
+    /// Check that the password contains a special character
+    private func containsSpecialCharacters() -> Bool {
+        
+        return false
     }
 }
 
