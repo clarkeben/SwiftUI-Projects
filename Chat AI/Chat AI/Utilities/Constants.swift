8ed3bd17ef7900229b809c03b3369b02e68eb106
clarke.ben <40464267+clarkeben@users.noreply.github.com>
2023-02-14 13:43:40 +0000

    Added keychainwrapper class to utilities to handle api key

---
+++ Chat AI/Chat AI/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Chat AI
+//
+//  Created by Ben Clarke on 14/02/2023.
+//
+
+import Foundation
+
+enum K {
+    enum Keychain {
+        static let apiKey = "APIKey"
+    }
+}
--- Chat AI/Chat AI/Views/Onboarding View/OnboardingView.swift
+++ Chat AI/Chat AI/Views/Onboarding View/OnboardingView.swift
@@ -5,30 +5,56 @@
 //  Created by Ben Clarke on 13/02/2023.
 //
 
+import AuthenticationServices
 import SwiftUI
 
 struct OnboardingView: View {
     //MARK: - Properties
     @State private var apiKey = ""
     
+    private let screenHeight = UIScreen.main.bounds.height
+    
     //MARK: - Body
     var body: some View {
         VStack {
-            Section {
-                Text("TITLE")
-                Text("Subtitle")
+            Image("robot")
+                .resizable()
+                .scaledToFill()
+                .frame(height: screenHeight/4)
+            
+            Spacer()
+            
+            Text("Welcome...")
+                .font(.largeTitle)
+            
+            Text("Subtitle")
+                .font(.subheadline)
+            
+            TextField("Enter API Key", text: $apiKey)
+                .dropShadowRoundView()
+                .padding(10)
+            
+            Button {
+                
+            } label: {
+                Text("Submit")
             }
-            Section("API Key") {
-                TextField("Enter API Key", text: $apiKey)
-                    .dropShadowRoundView()
-                    .padding(10)
-                Button {
-                    
-                } label: {
-                    Text("Submit")
-                }
+            
+            Spacer()
+            
+        }
+    }
+}
 
-            }
+//MARK: - ViewModel
+class OnboardingViewModel: ObservableObject {
+    // Properties
+    @Published var apiKey = ""
+    
+    // Methods
+    private func saveAPIKey() {
+        if apiKey != "" {
+            KeychainWrapper.standard.set(apiKey, forKey: K.Keychain.apiKey)
         }
     }
 }
