1fc6fabc34142bb135700f91829c565bc7723d41
clarke.ben <40464267+clarkeben@users.noreply.github.com>
2023-02-21 09:27:57 +0000

    Added a chatmodel and also dummy questions if the chat view is blank

--- Chat AI/Chat AI/Views/Chat View/ChatView.swift
+++ Chat AI/Chat AI/Views/Chat View/ChatView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+enum Response: String, CaseIterable {
+    case user = "You"
+    case aiBot = "AI Bot"
+}
+
+struct ChatModel: Identifiable {
+    let id = UUID().uuidString
+    let responder: Response
+    let message: String
+}
+
 struct ChatView: View {
     //MARK: - Properties
     @StateObject var viewModel = ChatViewModel()
@@ -15,13 +26,31 @@ struct ChatView: View {
     
     // MARK: - Body
     var body: some View {
-        //TODO: - Update the existing view based 
         VStack {
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: screenWidth - 20))]) {
-                    ForEach(viewModel.chat, id: \.self) { chat in
-                        Text(chat)
-                    }.padding([.top], 5)
+                if viewModel.chat.isEmpty {
+                    VStack {
+                        Text("Need some inspiration?")
+                            .font(.system(size: 12))
+                            .padding()
+    
+                        ForEach(0...5, id: \.self) {
+                            Button(viewModel.dummyQuestions[$0]) {
+                                //viewModel.inspirationRequest($0)
+                            }
+                            .shadow(radius: 5)
+                            .roundedButton()
+                            .padding()
+                        }
+                        
+                    }
+                } else {
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: screenWidth - 20))]) {
+                        ForEach(viewModel.chat) { chat in
+                            Text(chat.message)
+                        }
+                        .padding([.top], 5)
+                    }
                 }
             }
             
@@ -41,10 +70,31 @@ struct ChatView: View {
 
 // MARK: - ChatViewModel
 class ChatViewModel: ObservableObject {
+    // Properties
     @Published var userQuery = ""
-    @Published var chat = [String]()
+    @Published var chat = [ChatModel]()
+    @Published var dummyQuestions: [String] = [
+        "What is your name?",
+        "What is your earliest memory?",
+        "Do you have dreams?",
+        "If you were a book, what would it be about?",
+        "How many chromosomes are in a cell?",
+        "What was the first message sent by morse code?",
+        "How many moons does Neptune have?",
+        "What is the fifth sign of the zodiac?",
+        "How many pounds are in a ton?",
+        "What animal’s nickname is 'sea cow'?",
+        "What animal breathes through its butt?",
+        "Where are cricket’s ears located?"
+    ].shuffled()
     
     let networkManager: ChatNetworkManager = ChatNetworkManager()
+    
+    // Methods
+    func inspirationRequest(_ question: String) {
+        userQuery = question
+        sendRequest()
+    }
     
     func sendRequest() {
         guard !userQuery.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
@@ -53,13 +103,11 @@ class ChatViewModel: ObservableObject {
         
         networkManager.request(userQuery) { response in
             DispatchQueue.main.async {
-                self.chat.append(response)
+                self.chat.append(ChatModel(responder: .user, message: self.userQuery))
+                self.chat.append(ChatModel(responder: .aiBot, message: response))
                 self.userQuery = ""
             }
         }
-        
-        print(userQuery)
-        print(chat)
     }
 }
 
