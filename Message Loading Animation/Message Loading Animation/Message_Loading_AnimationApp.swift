4b93d52e07e3e6354d136fd999d0825e9c48ba5a
clarkeben <benclarke023@gmail.com>
2023-04-26 23:10:43 +0100

    Created the messaged loading animation file

---
+++ Message Loading Animation/Message Loading Animation/Message_Loading_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Message_Loading_AnimationApp.swift
+//  Message Loading Animation
+//
+//  Created by Benjamin Clarke on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Message_Loading_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
