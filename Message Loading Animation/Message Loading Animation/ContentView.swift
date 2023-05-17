5a194825713d74e33b9f4ead76e903555d01ec60
clarkeben <benclarke023@gmail.com>
2023-05-17 23:16:52 +0100

    Further modifications to animate the loading flicker

--- Message Loading Animation/Message Loading Animation/ContentView.swift
+++ Message Loading Animation/Message Loading Animation/ContentView.swift
@@ -13,38 +13,50 @@ struct ContentView: View {
     var body: some View {
         VStack {
             if isLoading {
-                VStack {
-                    LoadingBar(width: 100)
-                    LoadingBar(width: 80)
-                    LoadingBar(width: 60)
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Loading...")
+                        .offset(y: isLoading ? -50 : 0)
+                        .animation(.easeOut(duration: 0.5).repeatForever(), value: isLoading)
+                    LoadingBar(isLoading: $isLoading, width: 100)
+                    LoadingBar(isLoading: $isLoading, width: 80)
+                    LoadingBar(isLoading: $isLoading, width: 60)
                 }
                 .transition(.opacity)
-                .animation(.default)
-            } else {
-                Text("Tap to start")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .onTapGesture {
-                        withAnimation {
-                            isLoading.toggle()
-                        }
-                    }
+                .animation(.default, value: isLoading)
             }
         }
         .frame(width: 200, height: 100)
+        .preferredColorScheme(.dark)
+        .onAppear() {
+            withAnimation {
+                isLoading = true
+            }
+        }
     }
 }
 
 struct LoadingBar: View {
+    
+    @Binding var isLoading: Bool
+    
     let width: CGFloat
     let height: CGFloat = 20
     
+    private let darkGrey: Color = Color("DarkGrey")
+    private let lightGrey: Color = Color("LightGrey")
+    
     var body: some View {
         Rectangle()
-            .foregroundColor(.gray)
+            .foregroundColor(.clear)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [darkGrey, lightGrey]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+                .mask(Rectangle())
+                .animation(Animation.linear(duration: 0.8).repeatForever(), value: isLoading)
+            )
             .frame(width: width, height: height)
             .cornerRadius(10)
     }
