7ac6e675e18dd84dcf102c28d9a1214230d0444d
clarkeben <benclarke023@gmail.com>
2023-05-18 22:28:30 +0100

    Added a timer to the message loading animation and started playing around with the offset

--- Message Loading Animation/Message Loading Animation/ContentView.swift
+++ Message Loading Animation/Message Loading Animation/ContentView.swift
@@ -10,19 +10,25 @@ import SwiftUI
 struct ContentView: View {
     @State private var isLoading = false
     
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    
     var body: some View {
         VStack {
             if isLoading {
                 VStack(alignment: .leading, spacing: 10) {
                     Text("Loading...")
-                        .offset(y: isLoading ? -50 : 0)
+                        .offset(y:counter >= 5 ? 0 : -40)
                         .animation(.easeOut(duration: 0.5).repeatForever(), value: isLoading)
                     LoadingBar(isLoading: $isLoading, width: 100)
+                        .offset(y:counter >= 5 ? 0 : -40)
                     LoadingBar(isLoading: $isLoading, width: 80)
+                        .offset(y:counter >= 4 ? 0 : -40)
                     LoadingBar(isLoading: $isLoading, width: 60)
+                        .offset(y:counter >= 3 ? 0 : -40)
                 }
-                .transition(.opacity)
-                .animation(.default, value: isLoading)
+                .transition(.slide)
+                .animation(.easeInOut(duration: 0.7), value: isLoading)
             }
         }
         .frame(width: 200, height: 100)
@@ -31,6 +37,11 @@ struct ContentView: View {
             withAnimation {
                 isLoading = true
             }
+        }
+        .onReceive(timer) { timer in
+            counter += 1
+            
+            print(counter)
         }
     }
 }
@@ -55,7 +66,7 @@ struct LoadingBar: View {
                     endPoint: .trailing
                 )
                 .mask(Rectangle())
-                .animation(Animation.linear(duration: 0.8).repeatForever(), value: isLoading)
+                .animation(Animation.linear(duration: 0.6).repeatForever(), value: isLoading)
             )
             .frame(width: width, height: height)
             .cornerRadius(10)
