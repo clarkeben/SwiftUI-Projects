2291e253ef25102e9f43d82ff0c164aaf22a0641
clarkeben <benclarke023@gmail.com>
2023-09-29 23:58:57 +0100

    First initial commit for the pain scale slider

---
+++ Pain Scale Slider/Pain Scale Slider/Pain_Scale_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pain_Scale_SliderApp.swift
+//  Pain Scale Slider
+//
+//  Created by Benjamin Clarke on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pain_Scale_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
